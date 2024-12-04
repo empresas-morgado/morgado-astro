872b848ccce84cb9ba48112725bb383fddb16e04
Eduardo De Matos <62176573+nanudmr@users.noreply.github.com>
2024-12-04 13:56:59 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://nanudmr.github.io/morgado-astro/',
-  base: '/morgado-astro/',
-  integrations: [mdx(), sitemap()],
-});
+	site: 'https://nanudmr.github.io/morgado-astro/',
+	base: '/',
+  });
+  
