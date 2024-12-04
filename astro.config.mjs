cd3d93825f9a27e42fb07e2dbe7cbe8607811b14
Eduardo De Matos <62176573+nanudmr@users.noreply.github.com>
2024-12-04 13:42:25 -0400

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://nanudmr.github.io/morgado-astro/',
+  integrations: [mdx(), sitemap()],
+  base: '/morgado-astro/', // Set the base for GitHub Pages
+  outDir: 'dist', // Default output directory for Astro builds
 });
